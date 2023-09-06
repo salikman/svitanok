a25cf8d8c452bb754c4484a870c6fcdf639c8abc
Maxym Savonik <msavonik@salerow.software>
2023-09-06 11:44:48 +0300

    re home

--- src/js/app.js
+++ src/js/app.js
@@ -31,6 +31,27 @@ var swiper = new Swiper(".address-slider", {
   },
 });
 
+var reviews = new Swiper(".reviews-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  modules: [Navigation],
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1367: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
 // Включить/выключить FLS (Full Logging System) (в работе)
 window['FLS'] = location.hostname === 'localhost'
 
